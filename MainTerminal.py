eb5dcc909a78f5de1483d161d2b1c74105820571
jbnparmar <jashkumarparmar@gmail.com>
2023-05-23 13:55:18 +0100

    MainTerminal added to repo it's the program that will have input from out
    My_1text.txt has test relations
    
    RelationValueManipulationFunctions.py contains every function
    to maniputale relation value
    
    SelectionFunction.py - does the selection between to relation
    can be:
    - a > b
    - a < b
    - a = b, but thing and decide about this = selection result

---
+++ MainTerminal.py
@@ -0,0 +1,42 @@
+print("-------------------------------------------")
+print("Sakura Primary Terminal")
+# This name comes from a japanese novel and anime film adaptation callled "I want to eat your pancreas"
+# or in japanese called "Kimi no Suizou wo Tabetai" released in 2018
+print("Version: 1")
+print("First Prototype")
+print("-------------------------------------------")
+# This is chatbot first version with basic receiving functionality
+# The main terminal will only ever receive
+# Because human maind is reactive, and not proactive
+# To understand this better, use the abyss-fall theory
+
+from RelationValueManipulationFunctions import *
+from SelectionFuntion import *
+import gettext
+
+notDead = True
+
+while(notDead):
+    relation = input("Tell me something: ")
+    # priorityChart = open("Priority_Chart.txt", "a+")
+    # if(relation == priorityChart.readlines()):
+    #     priorityChart.write(relation + "again\n")
+    #     print(priorityChart.read())
+    #     priorityChart.seek(0) # this will put focus at top byte in file
+    # else:
+    #     priorityChart.write(relation + "\n")
+    #     priorityChart.seek(0) # this will put focus at top byte in file
+    #     print(priorityChart.readlines())
+    twoRelFunc(relation)
+
+
+
+    if(relation == "You are dead"):
+    # Delete this if, because it is here temporary to get out
+    # of loop without killing the process,
+    # but we can leave it here a bit modified, so one day
+    # it this program decides to destroy the world
+    # and let's hope that somehow this if statement survives that long
+    # so protagonist, not the hero, will come here and enter the fifth and the true answer
+    # to the fundamental question of the theory of cat and dog
+        notDead = False
---
+++ SelectionFuntion.py
@@ -0,0 +1,30 @@
+# This is the most important function, because
+# the ability to select or choose something for ourselves
+# is what is called intelligence, the more effienct solution to the
+# situetion, better the intelligence, but also,
+# what is true intelligence?
+
+from RelationValueManipulationFunctions import *
+
+# This function below allows only two relations to be selected
+def twoRelFunc(relation):
+    """I think we will only need two choices, because
+    even when it will have to make various choices, we can compare two at a time
+    Exemple: ordering number array in acending order
+    so, whenever, there are multiple chioce compare one on one, last one standing is selected"""
+    if(relation == "choice"):
+        print("twoRelFunc() running")
+        relOne = input("Give me option one: ")
+        relTwo = input("Give me option two: ")
+        if(findRelValue(relOne) > findRelValue(relTwo)):
+            print(f"I want { relOne }")
+        if(findRelValue(relOne) < findRelValue(relTwo)):
+            print(f"I want { relTwo }")
+        if(findRelValue(relOne) == findRelValue(relTwo)):
+            print("Is it possible to have to relations with exact same values?")
+            print("Not in this small version, but humans are always making judgements")
+            print("Can be based on anything, but let this == if statement be here")
+            print("Because, one day... this might be become a reality")
+            print("Only people with absolute and universally big reality will understand")
+            print("what truly means to have something with same value")
+            print("UPDATE: read Theory of cat and dog, to reconsider this")
