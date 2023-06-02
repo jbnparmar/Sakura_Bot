89afa73321ec407a60e60c2f5a0a27e7ffa84fff
jbnparmar <jashkumarparmar@gmail.com>
2023-06-02 15:54:49 +0100

    Exceptions added TypeError KeyError custom exception: RelationError, but failed more error...

--- RelationValueManipulationFunctions.py
+++ RelationValueManipulationFunctions.py
@@ -5,18 +5,36 @@ def import_test():
 
 def get_relation_table():
     """returns file text to avoid problems, when file name changes"""
-    with open("balde_de_relacoes.json", "r") as file:
-        rel = json.load(file)
-    return rel
+    try:
+        with open("balde_de_relacoes.json", "r") as file:
+            rel = json.load(file)
+        return rel
+    except FileNotFoundError as fnfe:
+        print("\n\n" + f"!!! ERROR Relation bucket file missing { fnfe } !!!")
 
 def get_rel_initial_value(name):
-    rel = get_relation_table()
-    return rel[name]["value"]
+    try:
+        rel = get_relation_table()
+        return rel[name]["value"]
+    except (KeyError, TypeError) as e:
+        print("\n\n!!!ERROR get_rel_initial_value(name) !!!")
+        print("Entered relation doesn't exists in the relation bucket")
+        print(e)
 
 def get_rel_count(name):
-    rel = get_relation_table()
-    return rel[name]["count"]
+    try:
+        rel = get_relation_table()
+        return rel[name]["count"]
+    except (KeyError, TypeError) as e:
+        print("\n\n!!!ERROR get_rel_count(name) !!!")
+        print("Entered relation doesn't exists in the relation bucket")
+        print(e)
 
 def get_rel_value(name):
-    value = float(get_rel_initial_value(name)/get_rel_count(name))
-    return value
+    try:
+        value = float(get_rel_initial_value(name)/get_rel_count(name))
+        return value
+    except TypeError as te:
+        print("\n\n!!!ERROR get_rel_value(name) !!!")
+        print("Entered relation doesn't exists in the relation bucket")
+        print(te)
