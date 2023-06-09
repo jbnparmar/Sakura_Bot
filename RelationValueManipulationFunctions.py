337bf7ac3f4838b3eeb4216336246a5006a975b3
jbnparmar <jashkumarparmar@gmail.com>
2023-06-09 15:59:46 +0100

    com class, mas não está a funcionar com o appendd ou assim

--- RelationValueManipulationFunctions.py
+++ RelationValueManipulationFunctions.py
@@ -5,11 +5,23 @@ def import_test():
 
 class Relation():
     def __init__(self, happiness_bucket):
-        self.rel = open(happiness_bucket, "a+")
+        self.happiness_bucket = happiness_bucket
+        self.odj = []
+        open(self.happiness_bucket, "w")
         self.name = ""
         self.value = 0.0
         self.count = 0.0
 
-    def set_rel_name(self, name):
+    def set_rel_name(self, name, value, count):
         self.name = name
-        self.rel.writelines(self.name)
+        self.value = value
+        self.count = count
+        self.odj.append({ self.name: { "value": self.value, "count": self.count}})
+        with open(self.happiness_bucket, "w") as f:
+            json.dump(self.odj, f, indent=4, separators=(',', ': '))
+            f.seek(0)
+
+    def get_rel_name(self):
+        with open(self.happiness_bucket, "r") as f:
+            temp_bucket = json.load(f)
+        return temp_bucket
--- class_test.py
+++ class_test.py
@@ -1,6 +1,10 @@
 from RelationValueManipulationFunctions import Relation
 
-rel = Relation("class_test.txt")
+rel = Relation("json_test.json")
 
-rel.set_rel_name("banana")
-rel.set_rel_name("cas")
+rel.set_rel_name("banana", 0.2, 5)
+
+rel.set_rel_name("apple", 0.6, 8)
+
+print(rel.get_rel_name())
+print(type(rel.get_rel_name()))
