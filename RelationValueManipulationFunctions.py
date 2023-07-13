3343c7773c9f1cabbf566f0bdbd9756e4adf9faa
jbnparmar <jashkumarparmar@gmail.com>
2023-07-13 12:39:23 +0100

    Now it is possible to add relations in .json file and even update them

--- RelationValueManipulationFunctions.py
+++ RelationValueManipulationFunctions.py
@@ -5,8 +5,25 @@ def import_test():
 
 class Relation:
     def __init__(self, bucket_name):
+        """Relation class can only read from existing file
+            to use file for the first time, create empty file
+            and {} this inside that file and just use following method to add relations
+            >>> add_rel()
+        """
         self.bucket_name = bucket_name
         self.dict = ""
-        with open(self.bucket_name, "a") as f:
-            self.dict 
+        with open(self.bucket_name, "r") as f:
+            self.dict = json.loads(f.read())
 
+    def get_bucket(self):
+        return self.dict
+
+    def get_rel(self, rel):
+        return self.dict[rel]
+
+    def add_rel(self, rel_name, rel_value, rel_count):
+        """Method an also be used to update value and count of any relation"""
+        with open(self.bucket_name, "w") as f:
+            dict_to_be_added = { rel_name: { "value": rel_value, "count": rel_count } }
+            self.dict.update(dict_to_be_added)
+            f.write(json.dumps(self.dict))
--- class_test.py
+++ class_test.py
@@ -1,8 +1,15 @@
-from RelationValueManipulationFunctions import Relation
+from RelationValueManipulationFunctions import *
 
 rel = Relation("json_test.json")
 
-rel.set_rel("apple", 0.6, 8)
+# print(rel.get_rel("apple"))
 
-print(rel.get_rel())
-print(rel.get_rel_value('apple'))
+rel.add_rel("banana", 0.1, 6)
+
+# print(rel.get_rel("banana"))
+
+print(rel.get_bucket())
+
+rel.add_rel("cat", 0.12, 14)
+
+print(rel.get_bucket())
